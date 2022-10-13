f044328d2f4db47dd33a7b76112540c63d663e19
ahah525 <ahah525@naver.com>
2022-10-13 11:20:22 +0900

    feat : 관리자(ADMIN 권한가진 회원)만 관리자 페이지 접근 가능

--- src/main/java/com/ll/exam/app__2022_10_11/app/home/controller/AdminHomeController.java
+++ src/main/java/com/ll/exam/app__2022_10_11/app/home/controller/AdminHomeController.java
@@ -1,5 +1,6 @@
 package com.ll.exam.app__2022_10_11.app.home.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/adm")
 public class AdminHomeController {
     @GetMapping("")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String showIndex() {
         return "redirect:/adm/home/main";
     }
 
     // 관리자 메인페이지
     @GetMapping("/home/main")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String showMain() {
         return "adm/home/main";
     }
