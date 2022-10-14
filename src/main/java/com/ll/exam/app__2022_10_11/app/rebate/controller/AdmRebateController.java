0f0e0687659accbc15a2ac734b50f488e745e4ae
ahah525 <ahah525@naver.com>
2022-10-14 13:01:01 +0900

    feat : 선택한 년월에 해당하는 날짜 범위 구하기

--- src/main/java/com/ll/exam/app__2022_10_11/app/rebate/controller/AdmRebateController.java
+++ src/main/java/com/ll/exam/app__2022_10_11/app/rebate/controller/AdmRebateController.java
@@ -1,9 +1,14 @@
 package com.ll.exam.app__2022_10_11.app.rebate.controller;
 
+import com.ll.exam.app__2022_10_11.util.Ut;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/adm/rebate")
@@ -11,7 +16,22 @@ public class AdmRebateController {
     // 정산 데이터 생성 페이지
     @GetMapping("/makeData")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public String showMakeDat() {
+    public String showMakeData() {
         return "adm/rebate/makeData";
+    }
+
+    // 정산 데이터 생성
+    @PostMapping("/makeData")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @ResponseBody
+    public String makeData(String yearMonth) {
+        int monthEndDay = Ut.date.getEndDayOf(yearMonth);
+
+        String fromDateStr = yearMonth + "-01 00:00:00.000000";
+        String toDateStr = yearMonth + "-%02d 23:59:59.999999".formatted(monthEndDay);
+        LocalDateTime fromDate = Ut.date.parse(fromDateStr);    // 해당 년월의 시작일
+        LocalDateTime toDate = Ut.date.parse(toDateStr);        // 해당 년월이 끝일시
+
+        return "fromDateStr : %s<br>toDateStr : %s".formatted(fromDateStr, toDateStr);
     }
 }
--- src/main/java/com/ll/exam/app__2022_10_11/util/Ut.java
+++ src/main/java/com/ll/exam/app__2022_10_11/util/Ut.java
@@ -10,10 +10,42 @@ import org.springframework.http.ResponseEntity;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Ut {
+    public static class date {
+
+        // 해당 년, 월의 마지막 일자 구하기
+        public static int getEndDayOf(int year, int month) {
+            String yearMonth = year + "-" + "%02d".formatted(month);
+
+            return getEndDayOf(yearMonth);
+        }
+
+        // 년, 월의 마지막 일자 구하기
+        public static int getEndDayOf(String yearMonth) {
+            LocalDate convertedDate = LocalDate.parse(yearMonth + "-01", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            convertedDate = convertedDate.withDayOfMonth(
+                    convertedDate.getMonth().length(convertedDate.isLeapYear()));
+
+            return convertedDate.getDayOfMonth();
+        }
+
+        // 날짜 문자열 -> 해당 패턴의 LocalDateTime 변환
+        public static LocalDateTime parse(String pattern, String dateText) {
+            return LocalDateTime.parse(dateText, DateTimeFormatter.ofPattern(pattern));
+        }
+
+        // 날짜 문자열 -> 디폴트 패턴의 LocalDateTime 변환
+        public static LocalDateTime parse(String dateText) {
+            return parse("yyyy-MM-dd HH:mm:ss.SSSSSS", dateText);
+        }
+    }
+
     private static ObjectMapper getObjectMapper() {
         return (ObjectMapper) AppConfig.getContext().getBean("objectMapper");
     }
